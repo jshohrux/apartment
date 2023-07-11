02f357df562a5658708741a9944b9b502fb073a2
jshohrux <shoxrux97jumanov@gmail.com>
2023-07-11 17:49:49 +0500

    commit

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -89,7 +89,7 @@
 </aside>
 
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script>
     $(document).ready(function() {
         $.ajax({
